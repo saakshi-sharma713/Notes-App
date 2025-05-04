e01f27987c64018e2e6ff729879aba6c64335b70
Saakshi Sharma <saakshi7466@gmail.com>
2025-05-04 16:31:46 +0530

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,33 @@
-import { useContext, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Note from './Component/Note'
-import Input from "./Component/Input"
-import { NotesContext } from './Context/Notes'
-import { Route,Routes } from 'react-router-dom'
-import Container from './Component/Container'
-import Pages from "./Component/Pages"
-import Card from "./Component/Card"
+import React, { Suspense, lazy, useContext, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Note from "./Component/Note";
+import Input from "./Component/Input";
+import { NotesContext } from "./Context/Notes";
+import { Route, Routes } from "react-router-dom";
+import Card from "./Component/Card";
+
+// Correct way to use lazy()
+const Container = lazy(() => import("./Component/Container"));
+const Pages = lazy(() => import("./Component/Pages"));
+
 function App() {
-  const [count, setCount] = useState(0)
-   const {notes,status,setstatus} =useContext(NotesContext);
-  console.log(notes);
-  console.log(status);
+  const [count, setCount] = useState(0);
+  const { notes, status, setstatus } = useContext(NotesContext);
+
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Container />}></Route>
-      <Route path="/:id" element ={<Pages />}></Route>
-    </Routes>
-    
-    { status ? <Card/>: ""}
+      <Suspense fallback={<div>Loading ...</div>}>
+        <Routes>
+          <Route path="/" element={<Container />} />
+          <Route path="/:id" element={<Pages />} />
+        </Routes>
+
+        {status ? <Card /> : null}
+      </Suspense>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
