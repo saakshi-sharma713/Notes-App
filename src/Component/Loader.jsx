0325be0e7dd4e4ef21897257170802fe71dc67c2
Saakshi Sharma <saakshi7466@gmail.com>
2025-05-04 16:41:42 +0530

    changes made

--- src/Component/Loader.jsx
+++ src/Component/Loader.jsx
@@ -3,15 +3,7 @@ import React from "react";
 const Loader = () => {
   return (
     <div style={{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      height: "100vh",
-      fontSize: "20px",
-      fontWeight: "bold",
-      borderTop:"10px",
-      borderRadius:"50%",
-      borderColor:"blue",
+      
     }} className="load">
     
     </div>
